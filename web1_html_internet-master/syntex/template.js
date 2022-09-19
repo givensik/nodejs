876a2184e1d8fcd10c2e667e1626ad4fc66fad9c
givensik <kjs4072@icloud.com>
2022-09-19 23:42:23 +0900

    ㅇㄹㄴㄹ

---
+++ web1_html_internet-master/syntex/template.js
@@ -0,0 +1,8 @@
+var a = 'givensik'
+var letter = `gasdgasdhgkjasdgas${a}agdsgasdgasd`
+
+console.log(letter);
+
+//리터럴 -> 정보를 표현하는 기호 
+// 벡틱 기호 : ` 이거는 한글일때는 ₩로 나오지만 영어로하면 `로 된다.
+// 그래서 `sdkjgklsadjgl ${a} dsfjsdklf` 이런식으로 하면 문자열 안에 변수를 넣을 수 있다. 이것이 템플릿 리터럴이다.
